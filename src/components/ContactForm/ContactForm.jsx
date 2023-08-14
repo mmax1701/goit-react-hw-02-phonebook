9e339843e5f631d5555a8aacf6e01d350a0d3d2a
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-08-14 16:53:03 +0300

    correct

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,34 +4,61 @@ import css from './ContactForm.module.css';
 
 export class ContactForm extends Component {
     state = {
-        contacts: [],
-        name: ''
-    };
-    
-    handleChange = e => {
-        this.setState({ name: e.target.value });
+        name: '',
+        number: '',
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        
+        this.props.onSubmit(this.state);
+        this.reset();
     };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.createUser({...this.state})
+    handleChange = event => {
+        this.setState({
+            [event.currentTarget.name]: event.currentTarget.value
+        })
+    }
+
+    reset = () => {
+        this.setState({ name: '', number: '',})
     }
 
     render() {
-        const { name } = this.state;
         return (
             <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    name={name}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    onChange={this.handleChange}
-                />
-            
-                <button type='submit'>Add contact</button>
+                <label>
+                    <p>Name</p>
+                    <input
+                        type="text"
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                </label>
+                <label>
+                    <p>Number</p>
+                    <input
+                        type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+                        title="Phone number must be entered in the format: '+99 999 999 99 9'. For example: '02 952 123 45 67'"
+                        required
+                        value={this.state.number}
+                        onChange={this.handleChange}
+                    />
+                </label>
+                
+                <button type="submit" className={css.btn}>Add contact</button>
             </form>
-        );
+        )
     }
+}
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,30 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
-export const ContactList = ({ name }) => {
+export const ContactList = ({ contactList, deleteContact }) => {
     return (
         <ul>
-            <li>
-                <p>{name}</p>
-                <p></p>
-            </li>
+            {contactList.map(
+                ({id, name, number}) => (
+                    <li key={id} className={css.list}>
+                        <span>{name}: {number}</span>
+                        <button type='button' onClick={() => { deleteContact(id) }}>Delete</button>
+                    </li>
+                )
+            )}
         </ul>
+        
     )
+}
+
+ContactList.propTypes = {
+    contactList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            number: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    deleteContact: PropTypes.func.isRequired
 }
